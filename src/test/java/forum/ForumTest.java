68b121a9c277de3c19eed1db7264bf968658dd41
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2021-05-07 22:51:06 -0300

    Teste classe Forum

--- src/test/java/forum/ForumTest.java
+++ src/test/java/forum/ForumTest.java
@@ -1,14 +1,51 @@
 package forum;
 import static org.mockito.Mockito.mock;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ForumTest {
+import com.fullteaching.backend.entry.Entry;
+import com.fullteaching.backend.forum.Forum;
 
+class ForumTest {
+	
+	Forum forum1;
+	private List<Entry> entries;
+	
+	@BeforeEach
+	public void testForum() {
+		this.forum1 = new Forum (true);
+		this.entries = forum1.getEntries();
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testId() {
+		assertNotNull(this.forum1.getId());
+		long Id = 69;
+		this.forum1.setId(Id);
+		
+		assertEquals(this.forum1.getId(), Id);
+	}
+	
+	@Test
+	public void testActivated() {
+		assertEquals(this.forum1.isActivated(), true);
+	}
+	
+	@Test
+	public void testEntries() {		
+		assertEquals(this.forum1.getEntries(), entries);
+	}
+		
+	@Test
+	public void testToString() {
+		String toString = "Forum[activated: \"" + forum1.isActivated() + "\", #entries: \"" + forum1.getEntries().size() + "\", #comments: \"" + 1 + "\"]";		
+		assertEquals(this.forum1.toString(), toString);
 	}
 
 }
